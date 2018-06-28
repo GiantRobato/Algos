28735c043ab21297c355633f8d6b8e7a6a96a3b3
Dustin Mendoza <penguindustin@gmail.com>
2018-06-27 19:36:44 -0700

    removed useless 0 value in range

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -16,7 +16,7 @@
 def bubble_sort(arr):
     for i in range(len(arr) - 1):
         swapped = False
-        for j in range(0,len(arr) - i - 1):
+        for j in range(len(arr) - i - 1):
             if arr[j] > arr[j+1]:          
                 tmp = arr[j]
                 arr[j] = arr[j+1]
